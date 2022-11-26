bf6178cd9b9dee0737769908c902a9ee57cd42c6
Bilal-Ehsan <bilalehsan933@gmail.com>
2022-11-26 17:18:51 +0000

    Updated form to use built-in components rather than standard HTML tags

--- Data/Grocery.cs
+++ Data/Grocery.cs
@@ -1,7 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Application.Data;
 
 public class Grocery
 {
+    [Required]
+    [StringLength(15, ErrorMessage = "Name must be no longer than 15 characters")]
     public string? Name { get; set; }
+
+    [Required]
+    [Range(1, 10000, ErrorMessage = "Valid price range is from £1 to £10,000")]
     public double Price { get; set; }
 }
